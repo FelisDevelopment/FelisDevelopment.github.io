63483c12ac911c150d1eb9fa34c39bf5fc08c467
Niunisas <55489629+niunisas@users.noreply.github.com>
2022-11-02 17:42:44 +0200

    chore: update logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ function HomepageHeader() {
     return (
         <div className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className={`container ${styles.container}`}>
-                <img src="img/felislogo.png" alt="felislogo" width={256} height={256} />
+                <img src="img/felislogofull.png" alt="felislogo" width={256} height={256} />
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
             </div>
